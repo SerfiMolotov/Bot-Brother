31bf8724e57139a6dbf681574dbfec4b0a7bd2e8
Serfi <122705689+SerfiMolotov@users.noreply.github.com>
2024-04-20 20:49:08 +0200

    jvais serrer j'arrive pas a ce que le bot rejoingne le voc

--- main.js
+++ main.js
@@ -5,41 +5,11 @@ const config = require("./config")
 bot.login(config.token)
 
 bot.on("ready", async () => {
-    bot.on("messageCreate", async message => {
-        if (message.content === "!ping" && !message.author.bot) {
-            message.reply("!ping")
-            // create a function to take a number between 1 and 10 and return a random number between 1 and 10
-            function randomNum() {
-                return Math.floor(Math.random() * 10) + 1
-            }
-            // transform to string
-            const randomNumString = randomNum().toString()
-            // send it to the channel
-            message.channel.send(randomNumString)
-        }
-    })
 
-    // create a command when user type !avatar send his avatar url in the channel
-    bot.on("messageCreate", async message => {
-        if (message.content === "!avatar" && !message.author.bot) {
-            message.reply(message.author.displayAvatarURL())
-        }
-    })
-
-    
-    // create a command to perform calculations
-    bot.on("messageCreate", async message => {
-        if (!message.author.bot && message.content.startsWith("!calculate")) {
-            // extract the calculation expression from the message content
-            const expression = message.content.slice(11).trim();
-
-            // evaluate the expression using the eval() function
-            try {
-                const result = eval(expression);
-                message.channel.send(`The result is: ${result}`);
-            } catch (error) {
-                message.channel.send("Invalid expression");
-            }
-        }
-    });
+//when somone say "attardé", the bot will reply whit the car.png photo
+bot.on("messageCreate", message => {
+    if (message.content === "attardé") {
+        message.channel.send({files: ["./car.png"]})
+    }
+})
 })
